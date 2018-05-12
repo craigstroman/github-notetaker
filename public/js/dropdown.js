21f0cfc7d35ab622827060a365bd448dc0f297c5
Craig Stroman <cstroman@Craigs-MBP.fios-router.home>
2018-05-12 15:47:19 -0400

    version 2.0.0

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,15 +1,22 @@
 (function() {
-  var toggle = document.getElementsByClassName('navbar-dropdown')[0];
-  var dropDown = document.getElementsByClassName('dropdown-menu')[0];
+  var toggleMenu = document.getElementsByClassName('navbar-dropdown')[0];
+  var toggleIcon = document.getElementsByClassName('icon')[0];
+  var dropDownMenu = document.getElementsByClassName('dropdown-menu')[0];
 
-  if (toggle !== undefined) {
-    toggle.addEventListener('click', function(e) {
-      if (dropDown.style.display === 'block') {
-        dropDown.style.display = 'none';
-      } else if (dropDown.style.display === 'none' || dropDown.style.display === '') {
-        dropDown.style.display = 'block';
+
+  if (toggleMenu !== undefined) {
+    toggleMenu.addEventListener('click', function(e) {
+      if (dropDownMenu.style.display === 'block') {
+        dropDownMenu.style.display = 'none';
+      } else if (dropDownMenu.style.display === 'none' || dropDownMenu.style.display === '') {
+        dropDownMenu.style.display = 'block';
       }
-
     });
+
+      window.addEventListener('click', function(e) {
+        if (!toggleMenu.contains(e.target) && !toggleIcon.contains(e.target) && !dropDownMenu.contains(e.target)) {
+          dropDownMenu.style.display = 'none';
+        }
+      });
   }
 })();
