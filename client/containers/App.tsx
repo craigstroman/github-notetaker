58a0c67ffcd450f03729e46ae5073d4ebfc62828
Craig Stroman <cstroman@gmail.com>
2023-02-04 14:15:31 -0500

    Changing app setup

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from '../store/store';
 import Main from './main/Main';
 
-ReactDOM.render(
-  <Main />,
-  document.getElementById('app'),
+const element = document.getElementById('app');
+const root = createRoot(element as HTMLDivElement);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
