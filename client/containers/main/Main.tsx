50a78819dd9a9e050a28ed193fd4f17fcceecf35
Craig Stroman <cstroman@gmail.com>
2023-02-04 14:15:20 -0500

    Adding routes to routes

--- client/containers/main/Main.tsx
+++ client/containers/main/Main.tsx
@@ -1,32 +1,27 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { store } from '../../store/store';
+import { Route, Routes } from 'react-router-dom';
 import Home from './home/Home';
 import Profile from '../profile/Profile';
 import { SearchGitHub } from '../../components/search-github/SearchGitHub';
 import './Main.scss';
 
 const Main = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <div className="container">
-        <nav className="navbar navbar-default" role="navigation">
-          <div className="row">
-            <div className="col-md-12 search-bar">
-              <SearchGitHub />
-            </div>
-          </div>
-        </nav>
-        <div className="row">
-          <div className="col-md-12">
-            <Route path="/" element={<Home />} />
-            <Route path="/profile/:username" element={<Profile />} />
-          </div>
+  <div className="container">
+    <nav className="navbar navbar-default" role="navigation">
+      <div className="row">
+        <div className="col-md-12 search-bar">
+          <SearchGitHub />
         </div>
       </div>
-    </BrowserRouter>
-  </Provider>
+    </nav>
+    <div className="row">
+      <div className="col-md-12">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
+      </div>
+    </div>
+  </div>
 );
 
 export default Main;
