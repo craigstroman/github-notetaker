1cae75c6061ca8f1def1f55d1056c7baa1e2ee5a
Craig Stroman <cstroman@gmail.com>
2023-02-25 14:30:47 -0500

    Converting component to React hooks

--- client/components/search-github/SearchGitHub.tsx
+++ client/components/search-github/SearchGitHub.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './SearchGitHub.scss';
 
 export const SearchGitHub: React.FC = () => {
-  console.log('SearchGitHub: ');
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState<string>('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (userName) {
+      navigate(`/profile/${userName}`);
+    }
+  };
 
   return (
     <div className="search-container">
       <div className="row">
-        <div className="col-md-12">
-          <form className="form-inline form-search">
+        <div className="col-md-12 text-center">
+          <form className="form-inline form-search" role="form" onSubmit={handleSubmit}>
             <div className="form-group">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Enter a username"
                 aria-label="Username search field"
+                onChange={(e) => setUserName(e.target.value)}
               />
             </div>
             <div className="form-group">
