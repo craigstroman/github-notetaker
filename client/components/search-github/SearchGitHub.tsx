f9c6ca962b8883b1c8c5d2dfa29f5f7322c4f309
Craig Stroman <cstroman@gmail.com>
2023-02-25 13:54:15 -0500

    Making updates

--- client/components/search-github/SearchGitHub.tsx
+++ client/components/search-github/SearchGitHub.tsx
@@ -4,5 +4,27 @@ import './SearchGitHub.scss';
 export const SearchGitHub: React.FC = () => {
   console.log('SearchGitHub: ');
 
-  return <div>Search GitHub</div>;
+  return (
+    <div className="search-container">
+      <div className="row">
+        <div className="col-md-12">
+          <form className="form-inline form-search">
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter a username"
+                aria-label="Username search field"
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-block btn-primary">
+                Search GitHub
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
