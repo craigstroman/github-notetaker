07733aa513c4332ab6e042fdaceb6ef98a3a4dc1
Craig Stroman <cstroman@gmail.com>
2023-02-25 13:15:02 -0500

    Updating configuration

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,8 @@ const filePath = path.join(__dirname, './public/js/');
 const fileName = 'bundle.js';
 
 const ESLintOptions = {
+  overrideConfigFile: path.resolve(__dirname, 'client/.eslintrc.js'),
+  context: path.resolve(__dirname, '/client'),
   extensions: ['js', 'jsx', 'ts', 'tsx'],
   exclude: ['/node_modules/'],
   emitError: true,
@@ -38,13 +40,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-            plugins: [['@babel/plugin-proposal-object-rest-spread']],
+            configFile: './client/.eslintrc.js',
           },
         },
       },
