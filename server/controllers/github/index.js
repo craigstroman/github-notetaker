e740f6191b40e848270a302e502e6024624b4ffe
Craig Stroman <cstroman@gmail.com>
2023-05-06 16:07:05 -0400

    Updating to async/await

--- server/controllers/github/index.js
+++ server/controllers/github/index.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-import { application } from 'express';
-import util from 'util';
-import { getNotes } from '../notes/index';
 
 require('dotenv').config();
 
@@ -10,72 +7,51 @@ const headers = {
   headers: {
     client_id: process.env.GITHUB_CLIENT_ID,
     client_secret: process.env.GITHUB_CLIENT_SECRET,
-    Authorization: process.env.GITHUB_TOKEN,
+    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
     'content-type': 'application/json',
-    'x-ratelimit-reset': '1598048562',
   },
 };
-let repoCount = 0;
 
-export function getProfile(req, res) {
+export async function getProfile(req, res) {
   const username = req.params.username;
   const FaceBookBioFixture = require('../../../fixtures/facebook/bio.json');
   const CraigStromanBioFixture = require('../../../fixtures/craigstroman/bio.json');
 
-  // if (process.env.NODE_ENV === 'development') {
-  //   res.send(CraigStromanBioFixture);
-  // } else {
-  axios
-    .get(`${apiURL}/users/${username}`, {
+  try {
+    const result = await axios.get(`${apiURL}/users/${username}`, {
       headers,
-    })
-    .then((resp) => {
-      repoCount = resp.data['public_repos'];
-      res.send(resp.data);
-    })
-    .catch((error) => {
-      if (error.message === 'Request failed with status code 404') {
-        res.status(404).send({
-          message: 'Not found',
-          status: 404,
-        });
-      } else if (error.message.indexOf('API rate limit exceeded')) {
-        res.status(500).send({
-          message: 'Rate limit exceeded.',
-          status: 500,
-        });
-      }
-      res.status(500).send({
-        error,
-        message: 'There was an error.',
-      });
     });
-  // }
+
+    const { data } = result;
+
+    res.status(200).send(data);
+  } catch (error) {
+    console.log('error: ');
+    console.log(error);
+
+    res.status(500).send(error);
+  }
 }
 
-export function getRepos(req, res) {
+export async function getRepos(req, res) {
   const username = req.params.username;
   const FaceBookReposFixture = require('../../../fixtures/facebook/repos.json');
   const CraigStromanRepos = require('../../../fixtures/craigstroman/repos.json');
 
   const url = `https://api.github.com/users/${username}/repos?page=1&per_page=100`;
 
-  axios
-    .get(url, {
+  try {
+    const result = await axios.get(url, {
       headers,
-    })
-    .then((resp) => {
-      res.send(resp.data);
-    })
-    .catch((error) => {
-      if (error.message === 'Request failed with status code 404') {
-        res.status(404).send({
-          message: 'Not found',
-          status: 404,
-        });
-      }
-      res.status(500).send({
-        error,
-      });
     });
+
+    const { data } = result;
+
+    res.status(200).send(data);
+  } catch (error) {
+    console.log('error: ');
+    console.log(error);
+
+    res.status(500).send(error);
+  }
 }
