645ba99ded6da8d8b41308fe72b4ead226b7a8f4
Craig Stroman <cstroman@gmail.com>
2023-05-06 15:56:25 -0400

    Updating API methods

--- client/components/notes/notes.API.ts
+++ client/components/notes/notes.API.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import { INote, Notes, INotesState, IAddNotes, IRemoveNotes } from './notesTypes';
+import { INote } from './notesTypes';
 
 const nodeEnv = process.env.NODE_ENV;
 const apiUrl = nodeEnv === 'production' ? '/api' : 'http://localhost:3000/api';
 
-export async function getNotes(repo: string): Promise<{ data: Notes }> {
+export async function getNotes(repo: string): Promise<{ data: INote[] }> {
   const url = `${apiUrl}/notes/${repo}`;
 
   const res = await axios.get(url);
@@ -20,11 +20,11 @@ export async function addNotes(repo: string, note: string): Promise<{ data: INot
   return { data: result.data };
 }
 
-export async function removeNotes(repo: string, noteId: number): Promise<{ data: number }> {
+export async function removeNotes(repo: string, noteId: string): Promise<{ data: string }> {
   const url = `${apiUrl}/notes/${repo}/${noteId}`;
 
   const result = await axios.delete(url);
-  let id = 0;
+  let id = '';
 
   if (result.status === 200) {
     id = result.data.id;
